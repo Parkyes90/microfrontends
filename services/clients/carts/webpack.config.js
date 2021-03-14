86fdd58adac2f7d3609365c68377e44814913f54
parkyes90 <parkyes90@gmail.com>
2021-03-14 23:24:40 +0900

    chore: add carts

---
+++ services/clients/carts/webpack.config.js
@@ -0,0 +1,21 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8092,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "carts",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./CartShow": "./src/index",
+      },
+    }),
+    new HTMLWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
