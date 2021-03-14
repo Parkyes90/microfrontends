3e4941546df99e257b086a5f1032ece6121bd0be
parkyes90 <parkyes90@gmail.com>
2021-03-14 22:56:51 +0900

    chore: implementation module federation

--- services/clients/container/webpack.config.js
+++ services/clients/container/webpack.config.js
@@ -1,11 +1,18 @@
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
   devServer: {
-    port: 8080,
+    port: 8090,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        products: "products@http://localhost:8091/remoteEntry.js",
+      },
+    }),
     new HTMLWebpackPlugin({
       template: "./public/index.html",
     }),
--- services/clients/products/webpack.config.js
+++ services/clients/products/webpack.config.js
@@ -1,11 +1,19 @@
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
   devServer: {
-    port: 8081,
+    port: 8091,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
     new HTMLWebpackPlugin({
       template: "./public/index.html",
     }),
