3c3287575d6a3deba27d8a6d61cf737f6f0d41af
parkyes90 <parkyes90@gmail.com>
2021-04-26 23:50:30 +0900

    feat: use marketing app in container

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
 export default () => {
-  return <h1>Hi there!</h1>;
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
