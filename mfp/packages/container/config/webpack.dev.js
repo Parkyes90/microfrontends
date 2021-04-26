5c97b444dc5afd155299ff14465b5991e9b934c8
parkyes90 <parkyes90@gmail.com>
2021-04-26 23:43:54 +0900

    chore: setup configure

--- mfp/packages/container/config/webpack.dev.js
+++ mfp/packages/container/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const devConfig = {
   mode: "development",
@@ -10,6 +11,12 @@ const devConfig = {
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { mount } from "marketing/MarketingApp";
 export default () => {
   return <h1>Hi there!</h1>;
 };
