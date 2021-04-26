c00cba6e20a86a41a96a70a96bc67124a94af8f3
parkyes90 <parkyes90@gmail.com>
2021-04-26 23:25:25 +0900

    feat: Assembly app component

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 // mount
 const mount = (el) => {
-  ReactDOM.render(<h1>Hi There</h1>, el);
+  ReactDOM.render(<App />, el);
 };
 // immediately
 if (process.env.NODE_ENV === "development") {
