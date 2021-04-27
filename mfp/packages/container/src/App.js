68f550962e0daa351bfc52b0c947f19270b7fe08
parkyes90 <parkyes90@gmail.com>
2021-04-27 23:08:33 +0900

    chore: change for ci test - 2

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi there2!</h1>
+      <h1>Hi there3!</h1>
       <hr />
       <MarketingApp />
     </div>
