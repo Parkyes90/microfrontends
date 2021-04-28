63a3b351cce6aea4addafc718d3a2182796a6f85
parkyes90 <parkyes90@gmail.com>
2021-04-28 23:07:44 +0900

    fix: fix prod setting

--- mfp/packages/marketing/config/webpack.prod.js
+++ mfp/packages/marketing/config/webpack.prod.js
@@ -10,7 +10,8 @@ const prodConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
+      name: "marketing",
+      filename: "remoteEntry.js",
       exposes: {
         "./MarketingApp": "./src/bootstrap",
       },
